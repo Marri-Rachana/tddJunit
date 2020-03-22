b6449c69fcffe04defd42e4886212d1dfcf00c8a
Marri-Rachana <56470546+Marri-Rachana@users.noreply.github.com>
2020-03-22 20:02:53 +0530

    Add files via upload

---
+++ src/main/java/www/rachana/junitandttd/App.java
@@ -0,0 +1,13 @@
+package www.rachana.junitandttd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
